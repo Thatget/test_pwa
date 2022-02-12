ffd8fa8921719a9bef376fe92298bdfd2e49c558
Blanka <blanka@mage-world.com>
2022-02-12 09:40:30 +0700

    switch to developer !

--- src/components/maincontents/Customer.vue
+++ src/components/maincontents/Customer.vue
@@ -4,23 +4,31 @@
 
 <script>
 import axios from 'axios'
+import { ref } from '@vue/reactivity'
 export default {
-  setup () {
+  async setup () {
     const baseUrl = 'https://startpwa.com/'
-    const userData = {
-      username: 'roni_cost@example.com',
-      password: 'roni_cost3@example.com'
-    }
-    const api = `${baseUrl}index.php/rest/V1/integration/customer/token`
-    axios.post(api, userData).then(response => {
+    const token = ref('')
+    // const userData = {
+    //   username: 'roni_cost@example.com',
+    //   password: 'roni_cost3@example.com'
+    // }
+    const api = `${baseUrl}/rest/V1/integration/customer/token?username=roni_cost@example.com&password=roni_cost3@example.com`
+    await axios.post(api).then(response => {
       console.log(response.data)
+      token.value = response.data
     })
+
+    const config = {
+      headers: { Authorization: `Bearer ${token.value}` }
+    }
+
+    await axios.get(`${baseUrl}rest/V1/customers/me`, config)
+      .then(response => {
+        console.log(response.data)
+      }).catch(console.log)
     return {
     }
   }
 }
 </script>
-
-<style>
-
-</style>
