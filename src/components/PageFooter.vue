6ef5bf095eee91343662f4cdeb190baabb43b828
Your Name <quyet.nguyen.7712826@gmail.com>
2022-02-16 13:28:34 -0500

    style home page

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,16 +1,29 @@
 <template>
-  <footer class="text-center" style="background: #f4f4f4;">
+  <footer style="background: #f4f4f4;">
       <div class="container p-4">
-          <div class="row">
-              <div class="col-3">
-                  <h6>About us</h6>
-                  <h6>Customer Service</h6>
-              </div>
-              <div class="col-3"></div>
-              <div class="col-6"></div>
-          </div>
+        <div class="row">
+            <div class="col-3">
+            <ul class="list-unstyled">
+                <li><a href="">About us</a></li>
+                <li><a href="">Customer Service</a></li>
+            </ul>
+            </div>
+            <div class="col-3">
+                <ul class="list-unstyled">
+                    <li><a href="">Search Terms</a></li>
+                    <li><a href="">Privacy and Cookie Policy</a></li>
+                    <li><a href="">Advanced Search</a></li>
+                    <li><a href="">Orders and Returns</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2"></div>
+            <div class="col-4">
+                <input type="email" class="form-control mr-sm-2" name="" placeholder="Enter your email address" id="">
+            </div>
+        </div>
       </div>
-      <div class="bg-dark text-white p-3">Copyright © 2013-present Magento, Inc. All rights reserved.</div>
+      <div class="bg-dark text-white p-3 text-center">Copyright © 2013-present Magento, Inc. All rights reserved.</div>
   </footer>
 </template>
 
